c9b5361c1d11c7b7a37e88a2259adcd782a9458c
Abdullah <24034@supnum.mr>
2026-04-25 22:54:22 +0000

    Initial commit

---
+++ demo/src/main/java/com/example/demo/Entity/Course.java
@@ -0,0 +1,37 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    public Course() {
+    }
+
+    public Course(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
